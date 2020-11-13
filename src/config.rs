b3d0e956bf57b2663ccadf5db1d4199170c7496a
morganamilo <morganamilo@archlinux.org>
2020-11-13 19:11:21 +0000

    Improve error message for unkown option

--- src/config.rs
+++ src/config.rs
@@ -525,7 +525,7 @@ impl Config {
             "SudoFlags" => self.sudo_flags.extend(split),
             "BatFlags" => self.bat_flags.extend(split),
             "FileManagerFlags" => self.fm_flags.extend(split),
-            _ => bail!("unknown option '{}'", key),
+            _ => bail!("unknown option '{}' in section [bin]", key),
         };
 
         Ok(())
@@ -620,7 +620,7 @@ impl Config {
             _ => ok2 = false,
         };
 
-        ensure!(ok1 || ok2, "unknown option '{}'", key);
+        ensure!(ok1 || ok2, "unknown option '{}' in section [options]", key);
         ensure!(ok1 || has_value, "option '{}' does not take a value", key);
         Ok(())
     }
