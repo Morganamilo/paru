918a943ccf3ea479f5af8cfd61764233f6318d8d
morganamilo <morganamilo@archlinux.org>
2022-11-13 04:13:01 +0000

    Add repo support to tests

--- tests/paru.rs
+++ tests/paru.rs
@@ -17,3 +17,14 @@ mod chroot {
     use crate::common::run_chroot as run;
     include!("common/tests.rs");
 }
+
+mod repo {
+    use crate::common::run_repo as run;
+    include!("common/tests.rs");
+}
+
+#[cfg(feature = "mock_chroot")]
+mod repo_chroot {
+    use crate::common::run_repo_chroot as run;
+    include!("common/tests.rs");
+}
