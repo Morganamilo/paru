4effcac4b6f75abff2b34dd1a29815e796ef2430
morganamilo <morganamilo@archlinux.org>
2022-11-21 01:03:07 +0000

    colour version when printing install

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::fmt::Write;
 
 use crate::config::Config;
 use crate::repo;
@@ -34,6 +35,21 @@ pub fn ymd(date: i64) -> String {
     date.format("%Y-%m-%d").to_string()
 }
 
+fn word_len(s: &str) -> usize {
+    let mut len = 0;
+    let mut chars = s.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        if c == '\x1b' && chars.peek() == Some(&'[') {
+            chars.by_ref().take_while(|c| c != &'m').count();
+        } else {
+            len += 1;
+        }
+    }
+
+    len
+}
+
 pub fn print_indent<S: AsRef<str>>(
     color: Style,
     start: usize,
@@ -52,7 +68,7 @@ pub fn print_indent<S: AsRef<str>>(
 
             if let Some(word) = iter.next() {
                 print!("{}", color.paint(word.as_ref()));
-                pos += word.as_ref().len();
+                pos += word_len(word.as_ref());
             }
 
             if iter.peek().is_some() && pos + sep.len() < cols {
@@ -62,14 +78,15 @@ pub fn print_indent<S: AsRef<str>>(
 
             while let Some(word) = iter.next() {
                 let word = word.as_ref();
+                let len = word_len(word);
 
-                if pos + word.len() > cols {
+                if pos + len > cols {
                     print!("\n{:>padding$}", "", padding = indent);
                     pos = indent;
                 }
 
                 print!("{}", color.paint(word));
-                pos += word.len();
+                pos += len;
 
                 if iter.peek().is_some() && pos + sep.len() < cols {
                     print!("{}", sep);
@@ -109,34 +126,63 @@ pub fn color_repo(enabled: bool, name: &str) -> String {
     col.paint(name).to_string()
 }
 
-fn base_ver(base: &Base, devel: &HashSet<String>) -> String {
+fn base_string(config: &Config, base: &Base, devel: &HashSet<String>) -> String {
+    let c = config.color;
+    let mut s = String::new();
+    write!(
+        &mut s,
+        "{}{}",
+        base.package_base(),
+        c.install_version.paint("-"),
+    )
+    .unwrap();
     if base.packages().any(|p| devel.contains(p)) {
-        let mut s = String::new();
-        Base::write_base(
-            &mut s,
-            base.package_base(),
-            "latest-commit",
-            base.packages(),
-        )
-        .unwrap();
-        s
+        write!(&mut s, "{}", c.install_version.paint("latest-commit")).unwrap();
     } else {
-        base.to_string()
+        write!(&mut s, "{}", c.install_version.paint(base.version())).unwrap();
     }
+
+    if !Base::base_is_pkg(base.package_base(), base.packages()) {
+        write!(&mut s, "(").unwrap();
+        let mut pkgs = base.packages();
+        write!(&mut s, "{}", pkgs.next().unwrap()).unwrap();
+        for pkg in pkgs {
+            write!(&mut s, " {}", pkg).unwrap();
+        }
+        write!(&mut s, ")").unwrap();
+    }
+    s
 }
 
-fn to_install(actions: &Actions, devel: &HashSet<String>) -> ToInstall {
+fn to_install(config: &Config, actions: &Actions, devel: &HashSet<String>) -> ToInstall {
+    let c = config.color;
+    let dash = c.install_version.paint("-");
+
     let install = actions
         .install
         .iter()
         .filter(|p| !p.make)
-        .map(|p| format!("{}-{}", p.pkg.name(), p.pkg.version()))
+        .map(|p| {
+            format!(
+                "{}{}{}",
+                p.pkg.name(),
+                dash,
+                c.install_version.paint(p.pkg.version().to_string())
+            )
+        })
         .collect::<Vec<_>>();
     let make_install = actions
         .install
         .iter()
         .filter(|p| p.make)
-        .map(|p| format!("{}-{}", p.pkg.name(), p.pkg.version()))
+        .map(|p| {
+            format!(
+                "{}{}{}",
+                p.pkg.name(),
+                dash,
+                c.install_version.paint(p.pkg.version().to_string())
+            )
+        })
         .collect::<Vec<_>>();
 
     let mut build = actions.build.clone();
@@ -147,7 +193,10 @@ fn to_install(actions: &Actions, devel: &HashSet<String>) -> ToInstall {
         }
     }
     build.retain(|b| b.package_count() != 0);
-    let build = build.iter().map(|p| base_ver(p, devel)).collect::<Vec<_>>();
+    let build = build
+        .iter()
+        .map(|p| base_string(config, p, devel))
+        .collect::<Vec<_>>();
 
     let mut make_build = actions.build.clone();
     for base in &mut make_build {
@@ -159,7 +208,7 @@ fn to_install(actions: &Actions, devel: &HashSet<String>) -> ToInstall {
     make_build.retain(|b| b.package_count() != 0);
     let make_build = make_build
         .iter()
-        .map(|p| base_ver(p, devel))
+        .map(|p| base_string(config, p, devel))
         .collect::<Vec<_>>();
 
     ToInstall {
@@ -175,7 +224,7 @@ pub fn print_install(config: &Config, actions: &Actions, devel: &HashSet<String>
 
     println!();
 
-    let to = to_install(actions, devel);
+    let to = to_install(config, actions, devel);
 
     if !to.install.is_empty() {
         let fmt = format!("{} ({}) ", tr!("Repo"), to.install.len());
