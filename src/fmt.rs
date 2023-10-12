03d32bf85d8257309f0d2e2f25b71f4d5d367b7e
SandaruKasa <sandarukasa@ya.ru>
2023-10-12 15:20:54 +0300

    Fix deprecated `from_utc`

--- src/fmt.rs
+++ src/fmt.rs
@@ -25,13 +25,13 @@ pub fn opt(opt: &Option<String>) -> String {
 
 pub fn date(date: i64) -> String {
     let date = NaiveDateTime::from_timestamp_opt(date, 0).unwrap();
-    let date = DateTime::<chrono::Utc>::from_utc(date, chrono::Utc);
+    let date = DateTime::<chrono::Utc>::from_naive_utc_and_offset(date, chrono::Utc);
     date.to_rfc2822()
 }
 
 pub fn ymd(date: i64) -> String {
     let date = NaiveDateTime::from_timestamp_opt(date, 0).unwrap();
-    let date = DateTime::<chrono::Utc>::from_utc(date, chrono::Utc);
+    let date = DateTime::<chrono::Utc>::from_naive_utc_and_offset(date, chrono::Utc);
     date.format("%Y-%m-%d").to_string()
 }
 
