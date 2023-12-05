81efb1f810f7265f6bf7cf8637a5103f6836f56a
morganamilo <morganamilo@archlinux.org>
2023-12-05 00:45:35 +0000

    Filter out pkgbuild repos for aur warnings
    Fixes #1099

--- src/install.rs
+++ src/install.rs
@@ -1250,7 +1250,8 @@ fn print_warnings(config: &Config, cache: &Cache, actions: Option<&Actions>) {
     }
 
     if config.args.has_arg("u", "sysupgrade") && config.mode.aur() {
-        let (_, pkgs) = repo_aur_pkgs(config);
+        let (_, mut pkgs) = repo_aur_pkgs(config);
+        pkgs.retain(|pkg| config.pkgbuild_repos.pkg(config, pkg.name()).is_none());
 
         warnings.missing = pkgs
             .iter()
