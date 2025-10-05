Morganamilo/paru

commit af078e8bdf62e614f394627435abca7ee6b660e4

Update dependencies

src/util.rs
@@ -4,14 +4,13 @@ use crate::repo;
 use std::cell::Cell;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{stdin, stdout, BufRead, Write};
+use std::io::{stderr, stdin, stdout, BufRead, Write};
 use std::ops::Range;
-use std::os::fd::{AsFd, AsRawFd};
+use std::os::fd::AsFd;
 
 use alpm::{Package, PackageReason};
 use alpm_utils::{AsTarg, DbListExt, Targ};
 use anyhow::Result;
-use nix::libc::{STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO};
 use nix::unistd::dup2;
 use tr::tr;
 
@@ -409,18 +408,20 @@ pub fn repo_aur_pkgs(config: &Config) -> (Vec<&alpm::Package>, Vec<&alpm::Packag
 }
 
 pub fn redirect_to_stderr() -> Result<File> {
-    let stdout = stdout().as_fd().try_clone_to_owned()?;
-    dup2(STDERR_FILENO, STDOUT_FILENO)?;
+    let mut stdout = stdout().as_fd().try_clone_to_owned()?;
+    dup2(stderr(), &mut stdout)?;
     Ok(File::from(stdout))
 }
 
 pub fn reopen_stdin() -> Result<()> {
     let file = File::open("/dev/tty")?;
-    dup2(file.as_raw_fd(), STDIN_FILENO)?;
+    let mut stdin = stdin().as_fd().try_clone_to_owned()?;
+    dup2(&file, &mut stdin)?;
     Ok(())
 }
 
 pub fn reopen_stdout(file: &File) -> Result<()> {
-    dup2(file.as_raw_fd(), STDOUT_FILENO)?;
+    let mut stdout = stdout().as_fd().try_clone_to_owned()?;
+    dup2(&file, &mut stdout)?;
     Ok(())
 }

Cargo.toml
@@ -15,48 +15,48 @@ rust-version = "1.80"
 
 
 [dependencies]
-alpm = "4.0.2"
+alpm = "4.0.3"
 alpm-utils = "4.0.1"
 aur-depends = "4.0.3"
 aur-fetch = "0.11.3"
 cini = "1.0.0"
 pacmanconf = "3.0.0"
 raur = "7.0.0"
-srcinfo = "1.2.0"
+srcinfo = "1.3.1"
 
 
 ansiterm = "0.12.2"
-anyhow = { version = "1.0.95", features = ["backtrace"] }
-chrono = { version = "0.4.39", default-features = false, features = ["clock"] }
-dirs = "5.0.1"
+anyhow = { version = "1.0.98", features = ["backtrace"] }
+chrono = { version = "0.4.41", default-features = false, features = ["clock"] }
+dirs = "6.0.0"
 futures = "0.3.31"
-globset = "0.4.15"
+globset = "0.4.16"
 htmlescape = "0.3.1"
-indicatif = "0.17.9"
-scraper = "0.22.0"
-nix = { version = "0.29.0", features = ["fs", "user"] }
+indicatif = "0.18.0"
+scraper = "0.23.1"
+nix = { version = "0.30.1", features = ["fs", "user"] }
 reqwest = { version = "0.11.0", features = ["gzip", "socks"] }
-rss = { version = "2.0.11", default-features = false }
-serde = { version = "1.0.217", features = ["derive"] }
-serde_json = "1.0.134"
+rss = { version = "2.0.12", default-features = false }
+serde = { version = "1.0.219", features = ["derive"] }
+serde_json = "1.0.140"
 smart-default = "0.7.1"
-tempfile = "3.14.0"
-terminal_size = "0.4.1"
-tokio = { version = "1.42.0", features = [
+tempfile = "3.20.0"
+terminal_size = "0.4.2"
+tokio = { version = "1.46.1", features = [
     "process",
     "macros",
     "rt-multi-thread",
 ] }
 url = "2.5.4"
-env_logger = "0.11.6"
-log = "0.4.22"
-async-trait = { version = "0.1.83", optional = true }
+env_logger = "0.11.8"
+log = "0.4.27"
+async-trait = { version = "0.1.88", optional = true }
 tr = "0.1.10"
-unicode-width = "0.2.0"
+unicode-width = "0.2.1"
 regex = "1.11.1"
-signal-hook = "0.3.17"
-bitflags = "2.6.0"
-toml = { version = "0.8.19", features = ["preserve_order"] }
+signal-hook = "0.3.18"
+bitflags = "2.9.1"
+toml = { version = "0.8.23", features = ["preserve_order"] }
 
 [profile.release]
 codegen-units = 1

commit d1dfbc48895a2a3b3fadc14569c3f2ffb5112b2e

Fix reading arguments from stdin and interactive menu
commit af078e8b broke std{in,out} redirection by
cloning the FDs before passing them to dup2.

This commit fixes this by using dup2_std{in,out}
which doesn't require the FDs to be owned.

Fixes: #1413

src/util.rs
@@ -6,12 +6,12 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{stderr, stdin, stdout, BufRead, Write};
 use std::ops::Range;
-use std::os::fd::AsFd;
+use std::os::fd::{AsFd, OwnedFd};
 
 use alpm::{Package, PackageReason};
 use alpm_utils::{AsTarg, DbListExt, Targ};
 use anyhow::Result;
-use nix::unistd::dup2;
+use nix::unistd::{dup2_stdin, dup2_stdout};
 use tr::tr;
 
 #[derive(Debug)]
@@ -407,21 +407,19 @@ pub fn repo_aur_pkgs(config: &Config) -> (Vec<&alpm::Package>, Vec<&alpm::Packag
     }
 }
 
-pub fn redirect_to_stderr() -> Result<File> {
-    let mut stdout = stdout().as_fd().try_clone_to_owned()?;
-    dup2(stderr(), &mut stdout)?;
-    Ok(File::from(stdout))
+pub fn redirect_to_stderr() -> Result<OwnedFd> {
+    let stdout = stdout().as_fd().try_clone_to_owned()?;
+    dup2_stdout(&stderr())?;
+    Ok(stdout)
 }
 
 pub fn reopen_stdin() -> Result<()> {
     let file = File::open("/dev/tty")?;
-    let mut stdin = stdin().as_fd().try_clone_to_owned()?;
-    dup2(&file, &mut stdin)?;
+    dup2_stdin(&file)?;
     Ok(())
 }
 
-pub fn reopen_stdout(file: &File) -> Result<()> {
-    let mut stdout = stdout().as_fd().try_clone_to_owned()?;
-    dup2(&file, &mut stdout)?;
+pub fn reopen_stdout<Fd: AsFd>(file: Fd) -> Result<()> {
+    dup2_stdout(file)?;
     Ok(())
 }